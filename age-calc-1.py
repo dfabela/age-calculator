73d1e6a8beb33051c14479531a584761869b0579
dfabela <32273101+dfabela@users.noreply.github.com>
2017-09-25 09:20:00 -0700

    Create age-calc-1.py

---
+++ age-calc-1.py
@@ -0,0 +1,15 @@
+#birthyearguesser and age calculator
+x = int(input('How old are you?(in numbers, not words): '))
+name= input('And what is your name?:')
+equat = x + 5
+print ("In five years" , name , ", you'll be, ", equat)
+birth_year=input('So, would you like to know your birth year? if so, says "yes". if not say "no".(case sensetive)')
+birth = 2017-x
+if birth_year == 'no':
+ print ("Then goodbye"+ name, ', you weirdo')
+else:
+ print ("your birth year is",birth,',',name)
+print ('thank you')
+  
+
+  
